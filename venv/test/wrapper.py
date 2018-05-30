24fd9a9df84e46a8cc19aa96c8bf073e47021278
maml <maml897@163.com>
2018-05-30 08:10:54 +0800

    i

---
+++ venv/test/wrapper.py
@@ -0,0 +1,16 @@
+def log(func):
+    def wrapper(*args, **kw):
+        print('call %s():' % func.__name__)
+        return func(*args, **kw)
+    return wrapper
+
+@log
+def now():
+    print('2015-3-25')
+
+
+print("====================")
+now1 = log(now)
+
+print(now1);
+now1()
